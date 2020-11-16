5bd774507d07083cf7fb06dced402ba9210577c1
Nikhil S N <73348331+nikhil02nayak@users.noreply.github.com>
2020-11-16 18:15:47 +0530

    Add files via upload

---
+++ Javasubstring.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Javasubstring {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        int start = in.nextInt();
+        int end = in.nextInt();
+        String todisplay = S.substring(start,end);
+        System.out.println(todisplay);
+    }
+}
